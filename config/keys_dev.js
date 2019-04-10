0feb0dbb0fd9c9dc96c5ba713bf069436e63c184
Tyler R. Olsen <Tyler@Tylers-MacBook-Air.local>
2019-04-10 17:06:40 -0600

    trying to migrate to a form, integrate my scrape

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://TylerOlsen:Shank1989@cluster0-tihhu.mongodb.net/test?retryWrites=true',
+    secretOrKey: 'SECRET'
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var Datastore = require('nedb');
 var db = new Datastore();
+var address = new Datastore({ filename: 'address.db', autoload: true});
 
 db.loadDatabase(function (err) {
     
 })
+
+"mongodb+srv://TylerOlsen:Shank1989@cluster0-tihhu.mongodb.net/test?retryWrites=true"
